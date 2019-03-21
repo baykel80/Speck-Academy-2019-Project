28e2524566eb36444fd84aec460f4f8100bf9fa9
Mihael Ištvanović <mistvanov@gmail.com>
2019-03-21 19:43:20 +0100

    Done task 5 in homework9

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ var hall =[
     {id:5,name:"Dvorana 5",reservation:{isReserved:true,reservedFrom:new Date(2019, 2, 12, 18),reservedUntil:new Date(2019, 2, 12, 20)}},
     {id:6,name:"Dvorana 6",reservation:{isReserved:true,reservedFrom:new Date(2019, 2, 14, 18),reservedUntil:new Date(2019, 2, 14, 20)}}
     ];
-
+var nameHalls =[];
+for(var i=0;i<6;i++){
+    nameHalls.push(hall[i].name);
+}
+//console.log(nameHalls);
 http.createServer(function (request, response) {
 
    response.writeHead(200, {'Content-Type': 'text/plain'});
