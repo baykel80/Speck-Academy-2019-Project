798812547f60b8e0643c04d08495e7158fe949b5
Mihael Ištvanović <mistvanov@gmail.com>
2019-03-21 19:30:25 +0100

    one task 4 in homework 9

---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var hall =[
+    {id:1,name:"Dvorana 1",reservation:{isReserved:false,reservedFrom:null,reservedUntil:null}},
+    {id:2,name:"Dvorana 2",reservation:{isReserved:true,reservedFrom:new Date(2019, 2, 11, 10),reservedUntil:new Date(2019, 2, 11, 12)}},
+    {id:3,name:"Dvorana 3",reservation:{isReserved:true,reservedFrom:new Date(2019, 2, 10, 18),reservedUntil:new Date(2019, 2, 10, 22)}},
+    {id:4,name:"Dvorana 4",reservation:{isReserved:false,reservedFrom:null,reservedUntil:null}},
+    {id:5,name:"Dvorana 5",reservation:{isReserved:true,reservedFrom:new Date(2019, 2, 12, 18),reservedUntil:new Date(2019, 2, 12, 20)}},
+    {id:6,name:"Dvorana 6",reservation:{isReserved:true,reservedFrom:new Date(2019, 2, 14, 18),reservedUntil:new Date(2019, 2, 14, 20)}}
+    ];
+
+http.createServer(function (request, response) {
+
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end('Hello World\n');
+}).listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/')
