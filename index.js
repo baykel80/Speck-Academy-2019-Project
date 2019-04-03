7c8f57c68d5a9c18ce9a289f28dc4739da486281
Mihael Ištvanović <mistvanov@gmail.com>
2019-04-03 10:46:38 +0200

    Done task 2 in homework 12

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express=require("express");
 var app=express();
+var arr = require('./halls.js');
 
-app.get("/",function(req,res){
-    res.send("HELLO");
+app.use("/halls",function(req,res){
+    res.json(arr);
 });
 
 app.listen(3000);
