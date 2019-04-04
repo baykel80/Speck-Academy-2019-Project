8069b33bcdd1629a43e495882238b1f1d1de9e57
Mihael Ištvanović <mistvanov@gmail.com>
2019-04-04 16:29:39 +0200

    Done task 6 in homework 12

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express=require("express");
 var app=express();
 var arr = require('./halls.js');
+var duljina=arr.length;
 
 app.use(express.static('public'));
 app.get("/",function(req,res){
@@ -10,5 +11,33 @@ app.get("/",function(req,res){
 app.use("/halls",function(req,res){
     res.json(arr);
 });
-
+app.use(express.urlencoded());
+app.post("/hallsCreate",function (req,res){
+    const formName = req.body.hallName;
+    arr[arr.length]={id:arr.length+1,name:formName,reservation:{isReserved:false,reservedFrom:null,reservedUntil:null}};
+    res.redirect("/halls");
+});
+app.post("/hallsUpdate",function (req,res){
+    var reservation=false;
+    if(req.body.isReserved=="isReserved"){
+        reservation=true;
+    }
+    for(var i=0;i<req.body.ID;i++){
+        if(arr[i].id==req.body.ID){
+            arr[i].reservation.isReserved=reservation;
+            arr[i].reservation.reservedUntil=req.body.reservedUntil;
+            break;
+        }
+    }
+    res.redirect("/halls");
+});
+app.post("/hallsDelete",function (req,res){
+    for(var i=0;i<arr.length;i++){
+        if(arr[i].id==req.body.ID){
+        arr.splice(i,1);
+        break;
+        }
+    }   
+    res.redirect("index.html");
+});
 app.listen(3000);
